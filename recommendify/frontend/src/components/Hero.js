c2f7ca5cacd5058b41e72b903e821930a7788c0f
Jacky Wong <jackyw2500@gmail.com>
2025-03-13 14:13:30 -0400

    added components folder for front-end

---
+++ recommendify/frontend/src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <section>
+            <div className="hero-content">
+                <h1>Welcome to Recommenify</h1>
+                <p>Click here to connect your Spotify account</p>
+                <button> Connect </button>
+            </div>
+        </section>
+    )
+}
+export default Hero
---
+++ recommendify/frontend/src/components/header.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+import './headerStyles.css'
+
+function Header() {
+    return (
+        <div className='header'>
+            <div>
+                <h2>RECOMMENDIFY</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Header;

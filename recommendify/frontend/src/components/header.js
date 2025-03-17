0022c0b8096cbd2fdbc169bec570de7f144f8259
Jacky Wong <jackyw2500@gmail.com>
2025-03-17 14:09:56 -0400

    added spotify icon to button

--- recommendify/frontend/src/components/header.js
+++ recommendify/frontend/src/components/header.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
 
 import './headerStyles.css'
+import SpotifyIcon from '../../../frontend/src/components/SpotifyIcon.png'
 
 function Header() {
+    const s = "Connect Account "
     return (
         <>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
             <span class="navbar-brand mb-0 h1">Recommendify</span>
             <button type="button" class="btn btn-success">
-                Connect account
+                {s} 
+                <img src = {SpotifyIcon} alt="buttonpng" border="0" width="25" height="25" />
             </button>
             </div>
         </nav>

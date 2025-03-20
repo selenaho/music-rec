06fb587b7772a92e7bb902b8a3c011a4e39c3e4e
Gordon M <gordon62105@gmail.com>
2025-03-20 17:29:08 -0400

    artists not working

--- recommendify/frontend/src/pages/Artists.js
+++ recommendify/frontend/src/pages/Artists.js
@@ -2,11 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 function Artists(){
     //get artist data
+    const [data, setData] = useState('');
+      useEffect(() => {
+        const fetchData = async () => {
+          const response = await fetch('spotify/top-artists');
+          const result = await response.json();
+          setData(result.url);
+          console.log(result);
+        };
     
+        fetchData();
+      }, []);
+
     return(
         <div>
+            {data}
             <p>These are your top artists</p>
-            {test}
         </div>
     )
 }

b0f854df35b5266c85f526bd2396e528c14a1e9e
selena <selenaho2000@gmail.com>
2025-04-11 16:37:54 -0400

    displaying all data

--- recommendify/frontend/src/index.js
+++ recommendify/frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './pages/App';
-import Artists from './pages/Artists';
+import Data from './pages/Data';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Main() {
@@ -10,7 +10,7 @@ export default function Main() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}/>
-        <Route path="/top-artists" element={<Artists />}/>
+        <Route path="/spotify-data" element={<Data />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ recommendify/frontend/src/pages/Data.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+function Data(){
+    //get top artist data
+    const [artistData, setArtistData] = useState([]);
+    //get top song data
+    const [songData, setSongData] = useState([]);
+    //get recent track data
+    const [recentTracks, setRecentTracks] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const artistResponse = await fetch('spotify/top-artists');
+            const artistResult = await artistResponse.json();
+            //console.log("result:")
+            setArtistData(artistResult);
+            //console.log(artistResult);
+
+            const songResponse = await fetch('spotify/top-tracks');
+            const songResult = await songResponse.json();
+            setSongData(songResult);
+
+            const recentResponse = await fetch('spotify/recently-played');
+            const recentResult = await recentResponse.json();
+            setRecentTracks(recentResult);
+        };
+        fetchData();
+    }, []);
+
+    
+
+    return(
+        <div>
+            <p>These are your top artists</p>
+            {artistData.map(d => <p>{d.name}: {d.followers} followers</p>)}
+            <p>These are your top tracks</p>
+            {songData.map(d => <p>{d.name} by {d.artist}</p>)}
+            <p>These are your recent listens</p>
+            {recentTracks.map(d => <p>{d.name} {d.artist}</p>)}
+            
+        </div>
+    )
+}
+
+export default Data;

6267612f924000f48282e75a08a0b11b0710f1c2
Gordon M <gordon62105@gmail.com>
2025-04-18 15:53:37 -0400

    worked on SongCard

--- recommendify/frontend/src/components/Hero.js
+++ recommendify/frontend/src/components/Hero.js
@@ -5,13 +5,13 @@ import Image from 'react-bootstrap/Image';
 
 import './HeroStyles.css'
 import iphone from './iphone_spotify.png'
-
+import PinkFloyd from './Pink-Floyd-Dark-Side-of-the-Moon.webp'
 import SongCard from './SongCard';
 
 function Frontground() {
     return (
         <section>
-            <Container className="container-fluid">
+            <Container fluid>
                 <Card className="card text-bg-light mb-3 text-center">
                     <Card.Body>
                         <Card.Title> Welcome to Recommendify </Card.Title>
@@ -23,9 +23,15 @@ function Frontground() {
         </section>
     )
 }
+
 function Background(){
     return(
-        <SongCard/>
+        <>
+        <SongCard albumCover  = {PinkFloyd} artistName={'Pink Floyd'} songTitle = {"Us and Them"}/>
+        {/* <SongCard albumCover  = {PinkFloyd} artistName={'Pink Floyd'} songTitle = {"Us and Them"}/>
+        <SongCard albumCover  = {PinkFloyd} artistName={'Pink Floyd'} songTitle = {"Us and Them"}/>
+        <SongCard albumCover  = {PinkFloyd} artistName={'blah blah blah blah blah'} songTitle = {"long title long title long title long title long title long title"}/> */}
+        </>
     )
 }
 
--- recommendify/frontend/src/components/SongCard.js
+++ recommendify/frontend/src/components/SongCard.js
@@ -1,20 +1,31 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-
+import Image from 'react-bootstrap/Image';
+import "./SongCardStyles.css"
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function SongCard({ albumCover, artistName, songTitle }){
     return(
         <>
-        <Container>
+        <Container fluid>
+            <div className="responsive-card">
             <Card>
                 <Card.Body>
-                <img src={albumCover} alt={`${songTitle} album cover`} className="album-cover" />
-                <div className="song-info">
-                    <h3 className="song-title">{songTitle}</h3>
-                    <p className="artist-name">{artistName}</p>
-                </div>
+                <Row xs ='auto'>
+                    <Col><Image src={albumCover} alt={`${songTitle} album cover`} className="album-cover" /></Col>
+                    <Col>
+                        <Row className="song-title text-truncate">
+                            {songTitle}
+                        </Row>
+                        <Row className="artist-name text-truncate">
+                            {artistName}
+                        </Row>
+                    </Col>
+                </Row>
                 </Card.Body>
             </Card>
+            </div>
         </Container>
         </>
     );
